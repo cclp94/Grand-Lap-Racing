0c1508567874038447371855e22d7f647460d953
Caio Paiva <cclp94@gmail.com>
2016-02-21 15:30:13 -0500

    Initial Commit

---
+++ GLRacing/kart.cpp
@@ -0,0 +1,6 @@
+#include "kart.h"
+
+
+
+kart::kart() {
+}
---
+++ GLRacing/kart.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <GLFW\glfw3.h>
+
+using namespace std;
+
+class kart {
+public:
+	vector <GLfloat> vertices;
+	int size;
+	kart();
+};
